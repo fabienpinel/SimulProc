d521b0f3b37fd3f1fdb179669d9c3cdb7dfd0e18
HRAF <elamraniachraf93@gmail.com>
2014-06-11 00:41:00 +0200

    amélioration de la fonction d'affichage

--- instruction.c
+++ instruction.c
@@ -28,8 +28,20 @@
 
  void print_condition(Instruction instr){
 
- 	printf("%s", condition_names[instr.instr_generic._regcond] );
+ 	printf("%s, ", condition_names[instr.instr_generic._regcond] );
  } 
+ 
+ 
+ //! Impression du code operande .
+/*!
+ * \param instr l'instruction corespondante 
+ */
+  void print_code_op(Instruction instr){
+
+	printf("%s ",cop_names[instr.instr_generic._cop]);
+}
+ 
+ 
 
  //! Impression d'un registre  .
 /*!
@@ -37,8 +49,9 @@
  */
 
  void print_registre(Instruction instr){
-
- 	printf("%d",instr.instr_generic._regcond );
+	if(instr.instr_generic._regcond>10)
+ 	printf("R%d ",instr.instr_generic._regcond );
+ 	printf("R0%d ",instr.instr_generic._regcond );
  }
 
  //! Impression de l'operande   .
@@ -48,13 +61,45 @@
 
  void print_operande(Instruction instr){
  	if(instr.instr_generic._immediate){
- 		printf("%s",instr.instr_immediate._value); // I = 1 => operande = val ; 
+ 		printf("#%d",instr.instr_immediate._value); // I = 1 => operande = val ; 
  	} else {
  		 if(instr.instr_generic._indexed){
- 		 	printf("%d[R%d"],instr.instr_indexed._offset,instr.instr_indexed._rindex ); // I=0 & X=1 => adr = (RX)+Offset 
+ 		 	printf("%d[R%d]",instr.instr_indexed._offset,instr.instr_indexed._rindex ); // I=0 & X=1 => adr = (RX)+Offset 
  		 } else {
- 		 	printf("%s@0x", instr.instr_absolute._address ); // I=0 & X=1 => adr = abs ;
+			
+ 		 	printf("@0x%.4x", instr.instr_absolute._address ); // I=0 & X=1 => adr = abs ;
 
  		 }
  	}
  }
+ 
+void print_instruction(Instruction instr, unsigned addr){
+	switch (instr.instr_generic._cop) {
+		case LOAD:
+		case STORE:
+		case ADD:
+		case SUB:
+			print_code_op(instr) ;
+			print_registre(instr); 
+			print_operande(instr);
+			break ; 
+			
+		case BRANCH : 
+		case CALL:
+			print_code_op(instr) ;
+			print_condition(instr); 
+			print_operande(instr);
+			break ; 
+	    case PUSH:
+		case POP:
+			print_code_op(instr) ; 
+			print_operande(instr);
+			break;
+		case ILLOP:
+		case NOP:
+		case RET:
+		case HALT:
+		print_code_op(instr) ; 
+			break;		
+		}
+}
