cedae80bbb9d236a3da46587e121ffcf66809beb
Camille BOINAUD <camille.boinaud@gmail.com>
2014-06-12 10:24:23 +0200

    Ajout test et modif indice pc

---
+++ Tests/test_nop.asm
@@ -0,0 +1,17 @@
+        TEXT 20
+
+main    EQU *
+		ADD R04, #2
+        NOP
+        NOP
+        ADD R04, #4
+        NOP
+        END
+
+        DATA 20
+
+        WORD 0
+        WORD 0
+        WORD 3
+
+        END
